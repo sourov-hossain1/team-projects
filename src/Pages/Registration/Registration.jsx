b7768f421e19ac396ac6c1e13769d839e239a103
Md Saiful Islam <sheikhmsip@gmail.com>
2023-09-11 04:19:48 +0600

    nothing

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -4,6 +4,9 @@ const Registration = () => {
     return (
         <div>
             <h3>This is registration page</h3>
+            <form action="">
+                <input type="text" name='name' placeholder='Type your name' />
+            </form>
         </div>
     );
 };
